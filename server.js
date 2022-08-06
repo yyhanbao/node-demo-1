ac216c7e6e39bd578df3f41521d5f12f67d97167
yaoyao <iduting@163.com>
2022-08-06 16:30:21 +0800

    修改内容

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ var server = http.createServer(function (request, response) {
     response.write(`
 		<!DOCTYPE html>
 		<head>
-		<link rel="stylesheet" href="/x">
+		<link rel="stylesheet" href="/style.css">
 		</head>
 		<body>
 		<h1>标题</h1>
@@ -36,7 +36,7 @@ var server = http.createServer(function (request, response) {
 		</html>
 		`);
     response.end();
-  } else if (path === "/x") {
+  } else if (path === "/style.css") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/css;charset=utf-8");
     response.write(`h1{color: red;}\n`);
